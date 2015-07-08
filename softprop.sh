7140ed2c02037852dc92fb445a50d9dd02503f2e
Sean <seanlaguna@gmail.com>
2015-07-08 02:02:18 -0500

    more tidying...fixed silly mistakes

--- softprop.sh
+++ softprop.sh
@@ -1,10 +1,14 @@
 FAKEHOME="`pwd`"
+if [ "$FAKEHOME" == "$HOME" ]; then
+    echo "Pointless to have fakehome be the same as home, silly goose!"
+    exit 1
+fi
 
-echo -e "\n# added by softprop.sh script\n. $FAKEHOME/.softproprc" >> $HOME/.bashrc
+echo -e "\n# added by softprop.sh script\n. $FAKEHOME/.bashrc" >> $HOME/.bashrc
 
-ln -s ~/.tmux.conf $FAKEHOME/.tmux.conf
-ln -s ~/.vimrc $FAKEHOME/.vimrc
-ln -s ~/.vim $FAKEHOME/.vim
-ln -s ~/.config/xfce4/terminal/terminalrc $FAKEHOME/terminalrc
-ln -s ~/.config/xfce4/terminal/terminalrc $FAKEHOME/accels.scm
+ln -s $HOME/.tmux.conf $FAKEHOME/.tmux.conf
+ln -s $HOME/.vimrc $FAKEHOME/.vimrc
+ln -s $HOME/.vim $FAKEHOME/.vim
+ln -s $HOME/.config/xfce4/terminal/terminalrc $FAKEHOME/terminalrc
+ln -s $HOME/.config/xfce4/terminal/terminalrc $FAKEHOME/accels.scm
 
